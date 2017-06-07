8b2a7aa57126b3a3c141d49402ee6038777be053
Teshima <fumihiro.teshima@nts.ricoh.co.jp>
2017-06-07 10:57:26 +0900

    add buttons js

--- js/button_action.js
+++ js/button_action.js
@@ -1,7 +1,9 @@
-function add()
+function add(array)
 {
+    for(var i = 0; i < array;i++){
     var div_element = document.createElement("div");
-    div_element.innerHTML = '<div><button onclick="add()">新たなボタン</button></div>';
+    div_element.innerHTML = '<div><button onclick=add(1)>新たなボタン'+i+'</button></div>';
     var parent_object = document.getElementById("button");
     parent_object.appendChild(div_element);
+}
 }
