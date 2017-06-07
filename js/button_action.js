c7f09fae5ca905252c1ff014079479829a2e7bcc
Teshima <fumihiro.teshima@nts.ricoh.co.jp>
2017-06-07 15:11:03 +0900

    button with no action

--- js/button_action.js
+++ js/button_action.js
@@ -1,9 +1,20 @@
-function add(array)
-{
-    for(var i = 0; i < array;i++){
-    var div_element = document.createElement("div");
-    div_element.innerHTML = '<div><button onclick=add(1)>新たなボタン'+i+'</button></div>';
-    var parent_object = document.getElementById("button");
-    parent_object.appendChild(div_element);
+function add(array) {
+    for (var i = 0; i < array; i++) {
+        var div_element = document.createElement("div");
+        div_element.innerHTML = '<div><button onclick=add(1)>新たなボタン' + i + '</button></div>';
+        var parent_object = document.getElementById("button");
+        parent_object.appendChild(div_element);
+    }
 }
+
+function setlist(frag) {
+    if (frag) {
+        for (var i = 0; i < array; i++) {
+            var div_element = document.createElement("div");
+            div_element.innerHTML = '<div><button onclick=add(1)>新たなボタン' + i + '</button></div>';
+            var parent_object = document.getElementById("button");
+            parent_object.appendChild(div_element);
+        }
+    }
+    else { var x = 1; }
 }
