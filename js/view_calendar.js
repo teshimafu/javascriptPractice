8608088346d95f05d0219f1ec5dab3f96175cf3e
Teshima <fumihiro.teshima@nts.ricoh.co.jp>
2017-06-09 14:04:12 +0900

    first commit

---
+++ js/view_calendar.js
@@ -0,0 +1,9 @@
+
+for (var i = 0; i < 1; i++) {
+document.write ('<ul>');
+  document.write ('<li>Html</li>');
+  document.write ('<li>CSS</li>');
+  document.write ('<li>Photoshop</li>');
+  document.write ('<li>Illustrator</li>');
+document.write ('</ul>');
+}
